3fe2fcc40aacb75477303879c6083fe042bdd78d
engineerturph <engineerturph@gmail.com>
2024-09-11 22:49:26 +0300

    hippo deleted

--- HIPPO_jax/hippo.py
+++ HIPPO_jax/hippo.py
@@ -1,16 +1,2 @@
 import pandas as pd 
 
-# Load the data
-data = pd.read_csv("data.csv")
-
-# Print the first 5 rows of the data
-print(data.head())
-
-# Print the shape of the data
-print(data.shape)
-
-# Print the columns of the data
-print(data.columns)
-
-# Print the data types of the columns
-print(data.dtypes)
