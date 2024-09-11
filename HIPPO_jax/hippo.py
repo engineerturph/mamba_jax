db8ffd6cf842881555f183fe59d3739f00238915
engineerturph <engineerturph@gmail.com>
2024-09-11 22:29:37 +0300

    deneme

--- HIPPO_jax/hippo.py
+++ HIPPO_jax/hippo.py
@@ -1,1 +1,16 @@
+import pandas as pd 
 
+# Load the data
+data = pd.read_csv("data.csv")
+
+# Print the first 5 rows of the data
+print(data.head())
+
+# Print the shape of the data
+print(data.shape)
+
+# Print the columns of the data
+print(data.columns)
+
+# Print the data types of the columns
+print(data.dtypes)
