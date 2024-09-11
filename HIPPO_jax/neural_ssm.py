d8b825d51caf8ee95778e44982715fad05ef329d
engineerturph <engineerturph@gmail.com>
2024-09-11 23:01:39 +0300

    neural ssm started

---
+++ HIPPO_jax/neural_ssm.py
@@ -0,0 +1,32 @@
+from functools import partial
+import jax
+import jax.numpy as np
+from flax import linen as nn
+from jax.nn.initializers import lecun_normal, normal
+from jax.numpy.linalg import eigh, inv, matrix_power
+from jax.scipy.signal import convolve
+
+def K_conv(Ab, Bb, Cb, L):
+    return np.array(
+        [(Cb @ matrix_power(Ab, l) @ Bb).reshape() for l in range(L)]
+    )
+
+def causal_convolution(u, K, nofft=False):
+    if nofft:
+        return convolve(u, K, mode="full")[: u.shape[0]]
+    else:
+        assert K.shape[0] == u.shape[0]
+        ud = np.fft.rfft(np.pad(u, (0, K.shape[0])))
+        Kd = np.fft.rfft(np.pad(K, (0, u.shape[0])))
+        out = ud * Kd
+        return np.fft.irfft(out)[: u.shape[0]]
+
+def log_step_initializer(dt_min=0.001, dt_max=0.1):
+    def init(key, shape):
+        return jax.random.uniform(key, shape) * (
+            np.log(dt_max) - np.log(dt_min)
+        ) + np.log(dt_min)
+
+    return init
+
+
